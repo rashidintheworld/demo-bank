482ee0808d222bae4acac89e54f8267f7bd9daf3
rashidintheworld <teymurlurashid@gmail.com>
2023-09-18 15:39:23 +0400

    added useful dependencies

---
+++ src/main/java/com/example/bankdemoproject/BankDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankDemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankDemoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bankdemoproject/BankDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankdemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankDemoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
