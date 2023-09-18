4b585235c321a553fa884f64af63bc07f06c92b5
rashidintheworld <teymurlurashid@gmail.com>
2023-09-18 18:27:59 +0400

    added Exceptions constants variables class

---
+++ src/main/java/com/example/bankdemoproject/exception/ExceptionConstants.java
@@ -0,0 +1,12 @@
+package com.example.bankdemoproject.exception;
+
+public class ExceptionConstants {
+    public static final Integer INTERNAL_EXCEPTION = 100;
+    public static final Integer INVALID_REQUEST_DATA = 101;
+    public static final Integer CUSTOMER_NOT_FOUND = 102;
+    public static final Integer ACCOUNT_NOT_FOUND = 103;
+    public static final Integer TRANSACTION_NOT_FOUND = 104;
+    public static final Integer DEBIT_ACCOUNT_NOT_FOUND = 105;
+    public static final Integer INVALID_AMOUNT = 106;
+    public static final Integer USER_NOT_FOUND = 107;
+}
