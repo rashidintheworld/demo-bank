475e8ee1099e3629a1086e203b49364e934e4a31
rashidintheworld <teymurlurashid@gmail.com>
2023-09-18 18:19:34 +0400

    added swagger ui for api docs

---
+++ src/main/java/com/example/bankdemoproject/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.example.bankdemoproject.config;
+
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.bankdemoproject.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("BANK DEMO API DOCUMENTATION")
+                .description("Descr:")
+                .version("1.1")
+                .build();
+    }
+}
