7a508130adf6dd93eea5253bd0a5a1c1e1852c03
rashidintheworld <teymurlurashid@gmail.com>
2023-09-20 13:19:41 +0400

    Modified - Open api swagger codes

--- src/main/java/com/example/bankdemoproject/config/SwaggerConfig.java
+++ src/main/java/com/example/bankdemoproject/config/SwaggerConfig.java
@@ -1,14 +1,13 @@
 package com.example.bankdemoproject.config;
-
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.ApiInfoBuilder;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Configuration
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
@@ -17,15 +16,9 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.bankdemoproject.controller"))
                 .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
-
-    private ApiInfo apiInfo(){
-        return new ApiInfoBuilder()
-                .title("BANK DEMO API DOCUMENTATION")
-                .description("Descr:")
-                .version("1.1")
                 .build();
     }
+
+
+
 }
