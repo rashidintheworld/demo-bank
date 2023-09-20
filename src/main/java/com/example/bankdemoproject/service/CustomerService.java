25078c32fe53ea99d3003e418d42340b194e487a
rashidintheworld <teymurlurashid@gmail.com>
2023-09-20 09:06:42 +0400

    Bug fix - change create method

--- src/main/java/com/example/bankdemoproject/service/CustomerService.java
+++ src/main/java/com/example/bankdemoproject/service/CustomerService.java
@@ -3,6 +3,7 @@ package com.example.bankdemoproject.service;
 import com.example.bankdemoproject.dto.request.ReqCustomer;
 import com.example.bankdemoproject.dto.respond.RespCustomer;
 import com.example.bankdemoproject.dto.respond.Response;
+import com.example.bankdemoproject.entity.Customer;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,9 +12,7 @@ public interface CustomerService {
     Response<List<RespCustomer>> getCustomerList();
 
     Response<RespCustomer> getCustomerById(Long id);
-
     Response addCustomer(ReqCustomer reqCustomer);
-
     Response updateCustomer(ReqCustomer reqCustomer);
 
     Response deleteCustomer(Long id);
