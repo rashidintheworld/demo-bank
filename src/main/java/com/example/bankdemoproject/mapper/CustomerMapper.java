d467fb005fa26503c0c932073c28fa01b11a5c05
rashidintheworld <teymurlurashid@gmail.com>
2023-09-23 10:47:29 +0400

    added mapping and determined target id for customer id

--- src/main/java/com/example/bankdemoproject/mapper/CustomerMapper.java
+++ src/main/java/com/example/bankdemoproject/mapper/CustomerMapper.java
@@ -4,9 +4,11 @@ import com.example.bankdemoproject.dto.request.ReqCustomer;
 import com.example.bankdemoproject.dto.respond.RespCustomer;
 import com.example.bankdemoproject.entity.Customer;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
     Customer dtoToCustomerEntity(ReqCustomer reqCustomer);
+    @Mapping(target = "customerId",source = "id")
     RespCustomer customerEntityToDto(Customer customer);
 }
